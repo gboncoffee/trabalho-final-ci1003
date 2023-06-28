9d49df038928da117dcb55afb09abde4379d505b
Gabriel G. de Brito <gabrielgbrito@icloud.com>
2023-06-28 00:16:35 -0300

    Adicionada fase 4

--- script.js
+++ script.js
@@ -1,9 +1,231 @@
 var num_fase2 = 0
 
-//
-// TODO: fase 3 e 4
-//
+const teste_de_einstein = {
+    paises: [ 'Inglaterra', 'Aústria', 'Estados Unidos da América', 'Estados Unidos da América' ],
+    nascimento: [ 1815, 1914, 1906, 1936 ],
+    legado: [
+        'Primeira pessoa do mundo a programar um computador.',
+        'Criou a tecnologia necessária para o Wi-Fi (sistema que permite computadores se comunicarem sem fios).',
+        'Criou a primeira linguagem de programação.',
+        'Programou os computadores do foguete Apollo XI, que levou o ser humano à Lua.'
+    ],
+    curiosidade: [
+        'Era filha de Lord Byron, o famoso escritor.',
+        'Trabalhava como atriz, e estudar ciência e inventar era seu hobbie',
+        'Tinha pequenos pedaços de fio que chamava nanossegundos, porque eles tinham exatamente o comprimento que a luz viaja em um nanossegundo.',
+        'Trabalhou como professora de matemática antes de programar.'
+    ],
+}
 
+// {{{
+function fase4() {
+    document.querySelector('#header-title').innerHTML = 'Fase 4: Quiz Final'
+
+    //
+    // TODO
+    //
+}
+// }}}
+
+// fase 3 {{{
+function testa_fase3() {
+    if (
+        document.querySelector('#pais-ada')           == teste_de_einstein.paises[0] &&
+        document.querySelector('#pais-lamarr')        == teste_de_einstein.paises[1] &&
+        document.querySelector('#pais-grace')         == teste_de_einstein.paises[2] &&
+        document.querySelector('#pais-maggie')        == teste_de_einstein.paises[3] &&
+        document.querySelector('#nascimento-ada')     == teste_de_einstein.nascimento[0] &&
+        document.querySelector('#nascimento-lamarr')  == teste_de_einstein.nascimento[1] &&
+        document.querySelector('#nascimento-grace')   == teste_de_einstein.nascimento[2] &&
+        document.querySelector('#nascimento-maggie')  == teste_de_einstein.nascimento[3] &&
+        document.querySelector('#legado-ada')         == teste_de_einstein.legado[0] &&
+        document.querySelector('#legado-lamarr')      == teste_de_einstein.legado[1] &&
+        document.querySelector('#legado-grace')       == teste_de_einstein.legado[2] &&
+        document.querySelector('#legado-maggie')      == teste_de_einstein.legado[3] &&
+        document.querySelector('#curiosidade-ada')    == teste_de_einstein.curiosidade[0] &&
+        document.querySelector('#curiosidade-lamarr') == teste_de_einstein.curiosidade[1] &&
+        document.querySelector('#curiosidade-grace')  == teste_de_einstein.curiosidade[2] &&
+        document.querySelector('#curiosidade-maggie') == teste_de_einstein.curiosidade[3]
+    ) {
+        fase4()
+    } else {
+        document.querySelector('#button').innerHTML = `
+        Errado! Tente novamente!<br>
+        <button type="button" onclick="testa_fase3()">Responder!</button>
+        `
+    }
+}
+
+function fase3() {
+    let itens       = ''
+    let paises      = ''
+    let nascimento  = ''
+    let legado      = ''
+    let curiosidade = ''
+
+    for (i = 0; i < 4; i++) {
+        itens += `<li>${teste_de_einstein.paises[i]}</li>`
+        paises += `<option value="${teste_de_einstein.paises[i]}">${teste_de_einstein.paises[i]}</option>`
+    }
+    for (i = 0; i < 4; i++) {
+        itens += `<li>${teste_de_einstein.nascimento[i]}</li>`
+        nascimento += `<option value="${teste_de_einstein.nascimento[i]}">${teste_de_einstein.nascimento[i]}</option>`
+    }
+    for (i = 0; i < 4; i++) {
+        itens += `<li>${teste_de_einstein.legado[i]}</li>`
+        legado += `<option value="${teste_de_einstein.legado[i]}">${teste_de_einstein.legado[i]}</option>`
+    }
+    for (i = 0; i < 4; i++) {
+        itens += `<li>${teste_de_einstein.curiosidade[i]}</li>`
+        curiosidade += `<option value="${teste_de_einstein.curiosidade[i]}">${teste_de_einstein.curiosidade[i]}</option>`
+    }
+
+    document.querySelector('#header-title').innerHTML = 'Fase 3: Teste de Einstein'
+    document.querySelector('#main-view').innerHTML = `
+        <div id="text">
+            <h2 id="pergunta">O teste consiste em relacionar cada um dos seguintes itens
+            à uma localização na tabela: </h2>
+            <ul>
+            ${itens}
+            </ul>
+            <p>
+            Você pode pesquisar na internet o que precisar! E siga as seguintes dicas:
+            <ul>
+                <li>Perceba que há duas cientistas nascidas nos Estados Unidos da América.</li>
+                <li>Hamilton foi professora de matemática.</li>
+                <li>A nascida em 1936 está ao lado da que chamava fios de "nanossegundos".</li>
+                <li>A criadora do Wi-Fi era uma atriz, e estudava ciência por hobbie.</li>
+                <li>As duas nascidas nos EUA estão lado a lado.</li>
+                <li>A atriz está ao lado da inglesa.</li>
+                <li>A criadora da primeira linguagem de programação está ao lado da cientista que programou o foguete Apollo XI.</li>
+            </ul>
+            </p>
+            <table>
+                <tr>
+                    <th></th>
+                    <th>Ada Lovelace</th>
+                    <th>Hedy Lamarr</th>
+                    <th>Grace Hopper</th>
+                    <th>Margaret Hamilton</th>
+                </tr>
+                <tr>
+                    <td>País de Origem</td>
+                    <td>
+                        <select id="pais-ada">
+                            <option value="none" selected></option>
+                            ${paises}
+                        </select>
+                    </td>
+                    <td>
+                        <select id="pais-lamarr">
+                            <option value="none" selected></option>
+                            ${paises}
+                        </select>
+                    </td>
+                    <td>
+                        <select id="pais-grace">
+                            <option value="none" selected></option>
+                            ${paises}
+                        </select>
+                    </td>
+                    <td>
+                        <select id="pais-maggie">
+                            <option value="none" selected></option>
+                            ${paises}
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Ano de Nascimento</td>
+                    <td>
+                        <select id="nascimento-ada">
+                            <option value="none" selected></option>
+                            ${nascimento}
+                        </select>
+                    </td>
+                    <td>
+                        <select id="nascimento-lamarr">
+                            <option value="none" selected></option>
+                            ${nascimento}
+                        </select>
+                    </td>
+                    <td>
+                        <select id="nascimento-grace">
+                            <option value="none" selected></option>
+                            ${nascimento}
+                        </select>
+                    </td>
+                    <td>
+                        <select id="nascimento-maggie">
+                            <option value="none" selected></option>
+                            ${nascimento}
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Legado</td>
+                    <td>
+                        <select id="legado-ada">
+                            <option value="none" selected></option>
+                            ${legado}
+                        </select>
+                    </td>
+                    <td>
+                        <select id="legado-lamarr">
+                            <option value="none" selected></option>
+                            ${legado}
+                        </select>
+                    </td>
+                    <td>
+                        <select id="legado-grace">
+                            <option value="none" selected></option>
+                            ${legado}
+                        </select>
+                    </td>
+                    <td>
+                        <select id="legado-maggie">
+                            <option value="none" selected></option>
+                            ${legado}
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Curiosidade</td>
+                    <td>
+                        <select id="curiosidade-ada">
+                            <option value="none" selected></option>
+                            ${curiosidade}
+                        </select>
+                    </td>
+                    <td>
+                        <select id="curiosidade-lamarr">
+                            <option value="none" selected></option>
+                            ${curiosidade}
+                        </select>
+                    </td>
+                    <td>
+                        <select id="curiosidade-grace">
+                            <option value="none" selected></option>
+                            ${curiosidade}
+                        </select>
+                    </td>
+                    <td>
+                        <select id="curiosidade-maggie">
+                            <option value="none" selected></option>
+                            ${curiosidade}
+                        </select>
+                    </td>
+                </tr>
+            </table>
+            <div id="button">
+                <button type="button" onclick="testa_fase3()">Responder!</button>
+            </div>
+        </div>
+    `
+}
+// }}}
+
+// fase 2 {{{
 function testa_fase2() {
     let numero_tentado = document.querySelector('#number-input').value
     if (numero_tentado < num_fase2) {
@@ -15,9 +237,9 @@ function testa_fase2() {
     }
 }
 
-function segunda_fase() {
+function fase2() {
     num_fase2 = Math.floor(Math.random() * 100)
-    document.querySelector('#header-title').innerHTML = 'Fase 2'
+    document.querySelector('#header-title').innerHTML = 'Fase 2: Busca Binária'
     document.querySelector('#main-view').innerHTML = `
         <div id="text">
             <h2 id="pergunta">Escolhi um número entre 1 e 100. Tente advinhar!</h2>
@@ -28,20 +250,22 @@ function segunda_fase() {
         </div>
     `
 }
+// }}}
 
+// {{{
 function testa_fase1() {
     if (document.querySelector('#number-input').value == 54) {
-        segunda_fase()
+        fase2()
     } else {
         document.querySelector('#pergunta').innerHTML = 'Errou! Tente novamente!'
     }
 }
 
-function primeira_fase() {
+function fase1() {
     let circle   = "<span style='color: red;'> &#x25cf;</span>"
     let square   = "<span style='color: blue;'> &#x25A0;</span>"
     let triangle = "<span style='color: yellow;'> &#x25b2;</span>"
-    document.querySelector('#header-title').innerHTML = 'Fase 1'
+    document.querySelector('#header-title').innerHTML = 'Fase 1: Álgebra'
     document.querySelector('#main-view').innerHTML = `
         <div id="text">
             <h2 id="pergunta">Qual a resposta?</h2>
@@ -65,6 +289,7 @@ function primeira_fase() {
         </div>
     `
 }
+// }}}
 
 function tela_inicial() {
     document.querySelector('#main-view').innerHTML = `
@@ -83,7 +308,7 @@ function tela_inicial() {
             culpa qui officia deserunt mollit anim id est laborum.
             </p>
             <div id="button">
-                <button type="button" onclick="primeira_fase()">Jogar!</button>
+                <button type="button" onclick="fase1()">Jogar!</button>
             </div>
         </div>
         <div id="image">
